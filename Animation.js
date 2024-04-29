355fe055d349672e527793039b02cdf8db3512ea
Jacob Hoopes <rjacobhoopes@gmail.com>
2024-04-28 23:52:15 -0400

    trying to reduce lag still

--- Animation.js
+++ Animation.js
@@ -13,32 +13,27 @@ var turnSpeed = 0.03;
 var touchSensitivity = 0.004;
 var zoomSensitivity = 2;
 
+// jump settings
+let initialJumpVelocity = 6; // initial jump velocity
+let jumpVelocity = 0; // How many pixels the camera should move vertically while in the air each time step
+let gravity = -0.4; // Number of pixels the jumpVelocity changes by each time step
+let standHeight = 50;
+let groundHeight = 150;
+
+// helpful settings for movement
 var previousTouch = [];
 var pinchDistance = 0;
 let midX = "none";
 let midY = "none";
 let viewState = "none";
 
-let cam1;
-let cam2;
-
-let currentCamera;
+let cam;
 
 let boxes = [];
-let edge;
-
-let room;
-let currentDims;
 
 let g1Dims = [300,150,1200]; // Gallery 1 Dimensions
 let wallOffset = 10; // The minimum distance from the camera to any wall
 
-let initialJumpVelocity = 6; // initial jump velocity
-let jumpVelocity = 0; // How many pixels the camera should move vertically while in the air each time step
-let gravity = -0.4; // Number of pixels the jumpVelocity changes by each time step
-let standHeight = 50;
-
-let shapes;
 
 let G2;
 let G2BoundaryPoints = [[150, 0, 600],[150, 0, -600],[-150, 0, -600],[-150, 0, -600]];
@@ -53,17 +48,15 @@ let arches;
 // Inset Camera Testing
 // let capture
 
-
+let doCamTexture = false;
 let camTexture;
-let cam3;
+
+let viewX, viewY, viewZ;
 // let camTexture2;
 
 
 let fraction = 0.6; // the fraction of the width of the screen taken up by the strange rect
-
-
 let rows = 0 // the number of rows of arches-blocks there are
-
 let numBoxes = 5
 
 function preload() {
@@ -75,71 +68,41 @@ function preload() {
 
 function setup() {
     var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-
-    // var canvas = createGraphics(windowWidth, windowHeight, WEBGL, document.getElementById("outerDiv"));
+    
     canvas.parent("outerDiv");
-    cam2 = createCamera();
-    cam2.perspective(PI/3, width / height, 5*sqrt(3), 10000*sqrt(3));
+    cam = createCamera();
+    cam.perspective(PI/3, width / height, 5*sqrt(3), 10000*sqrt(3));
 
     camTexture = createGraphics(width, height, WEBGL);
-    // camTexture = p5.Framebuffer()
-    // cam3 = createCamera();
-    // cam3.perspective(PI/3, width*2 / height, 5*sqrt(3), 10000*sqrt(3));
-    // camTexture.setCamera(cam3);
-    camTexture.cam3 = createCamera()
-    camTexture.cam3.perspective(PI/3, width / height, 5*sqrt(3), 10000*sqrt(3));
- 
-    currentCamera = 2;
+
+    setCamera(cam);
 
     for (let i = 0; i < numBoxes; i++) {
         boxes[i] = [[random()*400-200], [random()*400-200], [random()*400-200]];
     }
 
-    currentDims = g1Dims;
-    // makeGallery();
-
-
-    // makeComplexGallery([],0,0,0);
-    // capture = createCapture(VIDEO)
-    // capture.hide()
-
-
-
-    // Inset Camera Testing
-
-    // camera(0, 0, (height / 2) / tan(PI/6), 0, 0, 0, 0, 1, 0);
-    
-    // let fov = PI/3;
-    // let aspect = width / height;
-    // let near = 0.1;
-    // let far = 10000;
-    // let cameraX = 0;
-    // let cameraY = 0;
-    // let cameraZ = 21; // (height / 2) / tan(fov / 2);
-
-    // camTexture.perspective(fov, aspect, near, far);
-    // camTexture.camera(cameraX, cameraY, cameraZ, 0, 0, 0, 0, 1, 0)
-    // camTexture.cam3 = cam2;
-    // camTexture.cam3.perspective(fov, aspect, near, far);
-    // camTexture.cam3.camera(cameraX, cameraY, cameraZ, 0, 0, 0, 0, 1, 0)
-
-    setCamera(cam2);
-    // camTexture.setCamera(camTexture.cam3)
 
 }
 
 function draw() {
     background(255);
-    texture(camTexture)
-
 
     for (let i = 0; i < boxes.length; i ++) {
         push()
         translate(boxes[i][0]*1 + sin(frameCount/100 - 70*i) * 40, boxes[i][1]*1 + sin(frameCount/200 - 59*i) * 20, boxes[i][2]*1 + sin(frameCount/46 - 100*i) * 17);
-        edge = 40;
-        camTexture.texture(img)
-        box(edge)
+        texture(camTexture)
+        box(40)
         pop();
+    }
+
+    if (doCamTexture) {
+        for (let i = 0; i < boxes.length; i ++) {
+            camTexture.push()
+            camTexture.translate(boxes[i][0]*1 + sin(frameCount/100 - 70*i) * 40, boxes[i][1]*1 + sin(frameCount/200 - 59*i) * 20, boxes[i][2]*1 + sin(frameCount/46 - 100*i) * 17);
+            camTexture.texture(img)
+            camTexture.box(40)
+            camTexture.pop();
+        }
     }
 
     // controls the movement of the camera and various elements in the scene
@@ -192,7 +155,7 @@ function draw() {
     if (keyIsDown(16)) { // shift
         // camY += moveSpeed;
     }
-    if (keyIsDown(32) && (camY > currentDims[1]/2 - standHeight - 1)) { // spacebar
+    if (keyIsDown(32) && (camY > groundHeight/2 - standHeight - 1)) { // spacebar
         jumpVelocity = initialJumpVelocity;
     } else {
         jumpVelocity += gravity;
@@ -202,10 +165,7 @@ function draw() {
 
     ensureOnlyFloor()
 
-
-
-    cam2.camera(camX, camY, camZ, camX, camY, camZ-200, 0, 1, 0);
-
+    cam.camera(camX, camY, camZ, camX, camY, camZ-200, 0, 1, 0);
 
     // to place a stationary rect in the middle of the view
     push();
@@ -215,46 +175,33 @@ function draw() {
     rotateY(-camPan - PI/2) // oh my god Frankie was reversed the whole time and I didn't see it
     let s = fraction*23; // 23 fills the page
     // rect(-s*width/height/2, -s/2, s*width/height, s)
-    beginShape()
-    vertex(-s*width/height/2, -s/2, 0, width/2 - fraction*width/2, height/2 - fraction*height/2) // width/2, height/2 when fraction = 0 vs 0, 0 when fraction = 1
-    vertex(s*width/height/2, -s/2, 0, width/2 + fraction*width/2, height/2 - fraction*height/2)
-    vertex(s*width/height/2, s/2, 0, width/2 + fraction*width/2, height/2 + fraction*height/2) // width/2, height/2 when fraction = 0 vs width, height when fraction = 1
-    vertex(-s*width/height/2, s/2, 0, width/2 - fraction*width/2, height/2 + fraction*height/2)
-    // vertex(-s*width/height/2, -s/2, 0, fraction*width/2, fraction*height/2) // width/2, height/2 when fraction = 0 vs 0, 0 when fraction = 1
-    // vertex(s*width/height/2, -s/2, 0, width - fraction*width/2, fraction*height/2)
-    // vertex(s*width/height/2, s/2, 0, width - fraction*width/2, height - fraction*height/2) // width/2, height/2 when fraction = 0 vs width, height when fraction = 1
-    // vertex(-s*width/height/2, s/2, 0, fraction*width/2, height - fraction*height/2)
-    endShape(CLOSE)
+    if (doCamTexture) {
+        texture(camTexture)
+        beginShape()
+        vertex(-s*width/height/2, -s/2, 0, width/2 - fraction*width/2, height/2 - fraction*height/2) // width/2, height/2 when fraction = 0 vs 0, 0 when fraction = 1
+        vertex(s*width/height/2, -s/2, 0, width/2 + fraction*width/2, height/2 - fraction*height/2)
+        vertex(s*width/height/2, s/2, 0, width/2 + fraction*width/2, height/2 + fraction*height/2) // width/2, height/2 when fraction = 0 vs width, height when fraction = 1
+        vertex(-s*width/height/2, s/2, 0, width/2 - fraction*width/2, height/2 + fraction*height/2)
+        endShape(CLOSE)
+        }
     pop();
-
-    camTexture.background(255)
-    // camTexture.lights()
+    if (doCamTexture) {
+        camTexture.background(255)
+    }
     lights()
 
+    cam.pan(camPan);
+    cam.tilt(camTilt);
 
- 
-    cam2.pan(camPan);
-    cam2.tilt(camTilt);
+    if (doCamTexture) {
+        viewX = camX - sin(camPan) * cos(camTilt) * 200;
+        viewY = camY + sin(camTilt) * 200;
+        viewZ = camZ - cos(camPan) * cos(camTilt) * 200;
+        
+        camTexture.camera(camX, camY, camZ, viewX, viewY, viewZ, 0, 1, 0)
+        camTexture.perspective(PI/3, width / height, 5*sqrt(3), 10000*sqrt(3))
+    }
 
-    let viewX = camX - sin(camPan) * cos(camTilt) * 200;
-    let viewY = camY + sin(camTilt) * 200;
-    let viewZ = camZ - cos(camPan) * cos(camTilt) * 200;
-    
-    camTexture.camera(camX, camY, camZ, viewX, viewY, viewZ, 0, 1, 0)
-    // camTexture.camera(camX - 100 * sin(camPan), camY, camZ - 100 * cos(camPan), viewX - 100 * sin(camPan), viewY, viewZ - 100 * cos(camPan), 0, 1, 0)
-    camTexture.perspective(PI/3, width / height, 5*sqrt(3), 10000*sqrt(3))
-
-    // camTexture.camera(camX, camY, camZ, camX, camY, camZ-200, 0, 1, 0)
-    // camTexture.pan(camPan);
-
-    // cam2.camera(cameraX, cameraY, cameraZ, 0, 0, 0, 0, 1, 0)
-    // cam3.camera(cameraX, cameraY, cameraZ, 0, 0, 0, 0, 1, 0)
-    
-    
-    // ambientMaterial(231,4,255);
-    // to place a stationary rect in the middle of the view
-
-    // model(shapes);
     push()
     translate(0,75,0)
     strokeWeight(0.5)
@@ -276,34 +223,26 @@ function draw() {
     }
     pop()
 
-    camTexture.push()
-    camTexture.translate(0,75,0)
-    camTexture.strokeWeight(1)
-    camTexture.fill(255)
-    camTexture.scale(-60);
-    for (let i = 0; i < rows; i++) {
+    if (doCamTexture) {
         camTexture.push()
-        camTexture.translate(0,0,i*8-8*rows/2)
-        for (let j = 0; j < rows; j++) {
+        camTexture.translate(0,75,0)
+        camTexture.strokeWeight(1)
+        camTexture.fill(255)
+        camTexture.scale(-60);
+        for (let i = 0; i < rows; i++) {
             camTexture.push()
-            camTexture.translate(j*8-8*rows/2,0,0)
-            camTexture.normalMaterial()
-            camTexture.model(arches)
+            camTexture.translate(0,0,i*8-8*rows/2)
+            for (let j = 0; j < rows; j++) {
+                camTexture.push()
+                camTexture.translate(j*8-8*rows/2,0,0)
+                camTexture.normalMaterial()
+                camTexture.model(arches)
+                camTexture.pop()
+            }
             camTexture.pop()
         }
         camTexture.pop()
     }
-    camTexture.pop()
-
-    for (let i = 0; i < boxes.length; i ++) {
-        camTexture.push()
-        // camTexture.texture(camTexture)
-        camTexture.translate(boxes[i][0]*1 + sin(frameCount/100 - 70*i) * 40, boxes[i][1]*1 + sin(frameCount/200 - 59*i) * 20, boxes[i][2]*1 + sin(frameCount/46 - 100*i) * 17);
-        edge = 40;
-        camTexture.box(edge)
-        camTexture.pop();
-    }
-
 
     // values to help movement on mobile devices
     if (touches.length === 0) {
@@ -320,8 +259,8 @@ function draw() {
 }
 
 function ensureOnlyFloor() {
-    if (camY > currentDims[1]/2 - standHeight) {
-        camY = currentDims[1]/2 - standHeight;
+    if (camY > groundHeight/2 - standHeight) {
+        camY = groundHeight/2 - standHeight;
     }
     // if (camY < -currentDims[1]/2 + wallOffset) {
     //     camY = -currentDims[1]/2 + wallOffset;
@@ -342,29 +281,6 @@ function remainWithinBounds() {
     }
     if (camZ < -8*60*b) {
         camZ = 8*60*b
-    }
-}
-
-// room bounds for the simple room
-function ensureHitboxes() {
-    // room hitboxes
-    if (camX > currentDims[0]/2 - wallOffset) {
-        camX = currentDims[0]/2 - wallOffset;
-    }
-    if (camX < -currentDims[0]/2 + wallOffset) {
-        camX = -currentDims[0]/2 + wallOffset;
-    }
-    if (camY > currentDims[1]/2 - standHeight) {
-        camY = currentDims[1]/2 - standHeight;
-    }
-    if (camY < -currentDims[1]/2 + wallOffset) {
-        camY = -currentDims[1]/2 + wallOffset;
-    }
-    if (camZ > currentDims[2]/2 - wallOffset) {
-        camZ = currentDims[2]/2 - wallOffset;
-    }
-    if (camZ < -currentDims[2]/2 + wallOffset) {
-        camZ = -currentDims[2]/2 + wallOffset;
     }
 }
 
@@ -411,16 +327,17 @@ function mouseDragged() {
         }
         pinchDistance = newPinchDistance;
     }
-    ensureHitboxes();
+    ensureOnlyFloor();
 }
 
+// code to adjust the number of arches
 function keyPressed() {
-    if (keyCode === 75) {
+    if (keyCode === 75) { // k
         rows --
         if (rows <= 0) {
             rows = 0;
         }
-    } else if (keyCode === 76) {
+    } else if (keyCode === 76) { // l
         rows ++
         if (rows >= 7) {
             rows = 7;
@@ -432,16 +349,17 @@ function mouseReleased() {
     exitPointerLock();
 }
 
-function doubleClicked() {
-    if (currentCamera === 1) {
-        setCamera(cam2);
-        currentCamera = 2;
-    } else if (currentCamera === 2){
-        setCamera(camTexture.cam3);
-        currentCamera = 1;
-    }
-}
-
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+    width = windowWidth;
+    height = windowHeight;
+    cam.perspective(PI/3, width / height, 5*sqrt(3), 10000*sqrt(3));
+    if (doCamTexture) {
+        camTexture.perspective(PI/3, width / height, 5*sqrt(3), 10000*sqrt(3))
+    }
+
+}
+
+function doubleClicked() {
+    doCamTexture = !doCamTexture;
 }
