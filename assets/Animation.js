7cc27b5094ab8b13fc8f430c293a82d6026e32af
Jacob Hoopes <100400340+JacobHoopes@users.noreply.github.com>
2025-02-13 12:34:47 -0800

    fixed angle and commented out inner rect

--- assets/Animation.js
+++ assets/Animation.js
@@ -3,7 +3,7 @@ var camY = 0;
 var camZ = 0;
 var camTilt = 0;
 var maxCamTilt = Math.PI / 2.001; // div by 2.001 limits view to straight up and down
-var camPan = 0;
+var camPan = -Math.PI/10;
 
 // settings for pc
 var moveSpeed = 4;
@@ -48,8 +48,8 @@ let fraction = 0.5; // the fraction of the width of the screen taken up by the s
 let rows = 1 // the number of rows of arches-blocks there are
 let numBoxes = 0
 
-let roomRadius = 350; // room radius is 400
-let hallWidth = 10;
+let roomRadius = 340; // room radius is 400
+let hallWidth = 100;
 
 let lightColor = color(255,255,255);
 
@@ -57,7 +57,7 @@ let lightColor = color(255,255,255);
 let quadrant = 0;
 
 function preload() {
-    arches = loadModel('./assets/arches.obj');
+    // arches = loadModel('./assets/arches.obj');
     room = loadModel('./assets/room-simple.obj');
     hall = loadModel('./assets/hall-simple.obj');
 }
@@ -91,7 +91,7 @@ function draw() {
     background(color(135,206,235));
 
     updateCamera();
-    addInnerRect();
+    // addInnerRect();
     scale(Scale);
 
     model(room);
@@ -234,17 +234,17 @@ function radialDistance(xPos, zPos, xCenter, zCenter) {
 
 function reduceDistance(cX, cZ, newD, xCenter, zCenter) {
     let quadrant = 0;
-    let angle = atan(camX/camZ) - HALF_PI;
-    if (camX >= 0 && camZ >= 0) { // quadrant 1
+    let angle = atan(cX/cZ) - HALF_PI;
+    if (cX >= 0 && cZ >= 0) { // quadrant 1
         angle = abs(angle);
         quadrant = 1;
-    } else if (camX < 0 && camZ > 0) { // quadrant 2
+    } else if (cX < 0 && cZ > 0) { // quadrant 2
         angle = abs(angle);
         quadrant = 2;
-    } else if (camX <= 0 && camZ <= 0) { // quadrant 3
+    } else if (cX <= 0 && cZ <= 0) { // quadrant 3
         angle = PI - angle;
         quadrant = 3;
-    } else if (camX > 0 && camZ < 0) { // quadrant 4
+    } else if (cX > 0 && cZ < 0) { // quadrant 4
         angle = PI - angle;
         quadrant = 4;
     }
